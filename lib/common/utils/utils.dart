ca95222ec38282dac10ce0280e693e2ac1b905a6
osung <osung5633@gmail.com>
2023-12-08 11:08:39 +0900

    refactor: 안쓰는 코드 제거

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -2,4 +2,3 @@ export 'size.dart';
 export 'app_update_utils.dart';
 export 'device_info_helper.dart';
 export 'preference_utils.dart';
-export 'size.dart';
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  firebase_auth
+  firebase_core
   permission_handler_windows
 )
 
